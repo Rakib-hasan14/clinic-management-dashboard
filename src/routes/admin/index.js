0c4591128dcea3470fc64e6ce1e464423d174236
Rakib-spickolab <rakib@spickolab.net>
2022-07-12 03:32:27 +0600

    update route

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -8,13 +8,13 @@ const {checkToken} = require('../../authentication/checkToken')
  *
  */
 
-router.use('/main', require('./endPoint/main.route.js'));
-router.use('/auth', require('./endPoint/auth.route.js'));
-router.use('/account', require('./endPoint/account.route.js'));
-router.use('/appointment', require('./endPoint/appointment.route.js'));
-router.use('/doctors', require('./endPoint/doctors.route.js'));
-router.use('/staffs', require('./endPoint/staff.route.js'));
-router.use('/pateints', require('./endPoint/pateint.route.js'));
-router.use('/profile', checkToken, require('./endPoint/profile.route.js'));
+router.use('/main', require('./endPoint/main.route'));
+router.use('/auth', require('./endPoint/auth.route'));
+router.use('/account', require('./endPoint/account.route'));
+router.use('/appointment', require('./endPoint/appointment.route'));
+router.use('/doctors', require('./endPoint/doctors.route'));
+router.use('/staffs', require('./endPoint/staff.route'));
+router.use('/pateints', require('./endPoint/pateint.route'));
+router.use('/profile', checkToken, require('./endPoint/profile.route'));
 
 module.exports = router
