d688cef23fac6d1bb882879f12cdbc15e9aaa63a
Rakib-spickolab <rakib@spickolab.net>
2023-09-23 00:45:23 +0600

    Add await in mongose connection

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,18 +3,20 @@ const mongoose = require('mongoose');
  * MongoDB Connection
  */
 
-await mongoose
-    .connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        //don't show the log when it is test
-        if (process.env.NODE_ENV !== 'development') {
-            console.log('Connected to %s', MONGODB_URL);
-        }
-    })
-    .catch(err => {
-        console.error('App starting error:', err.message);
-        // process.exit(1);
-    });
+(async () => {
+    await mongoose
+        .connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            //don't show the log when it is test
+            if (process.env.NODE_ENV !== 'development') {
+                console.log('Connected to %s', MONGODB_URL);
+            }
+        })
+        .catch(err => {
+            console.error('App starting error:', err.message);
+            // process.exit(1);
+        })
+});
--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
 const express = require('express');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { normalizePort } = require('../helpers/utility');
